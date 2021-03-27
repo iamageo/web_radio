d06ad50b44e8a76f9cc69c4eb5fa578ac972c56e
iamageo <geovani.af4@gmail.com>
2021-03-27 15:58:33 -0300

    add splashscreen in application

---
+++ app/src/main/java/com/iamageo/conexaopororocav1/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.iamageo.conexaopororocav1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }, 3000);
+
+
+    }
+    /* methods here */
+
+}
