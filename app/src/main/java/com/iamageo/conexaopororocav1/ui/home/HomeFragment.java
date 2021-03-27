55206227ed5cf5ae77e3e2e32fd609fc39d5046a
iamageo <geovani.af4@gmail.com>
2021-03-27 16:52:18 -0300

    change sdk version and app play function

--- app/src/main/java/com/iamageo/conexaopororocav1/ui/home/HomeFragment.java
+++ app/src/main/java/com/iamageo/conexaopororocav1/ui/home/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.iamageo.conexaopororocav1.ui.home;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,22 +18,103 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.iamageo.conexaopororocav1.R;
 
+import java.io.IOException;
+
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private Button btn_play;
+    private TextView text_status;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    String stream = "https://s1.guaracast.com:8427/stream";
+
+    boolean prepared = false;
+    boolean started = false;
+
+    MediaPlayer mediaPlayer;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+        btn_play = root.findViewById(R.id.btn_play);
+        text_status = root.findViewById(R.id.status);
+
+        btn_play.setEnabled(false);
+        text_status.setText("Loading..");
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        new PlayerTask().execute(stream);
+
+        btn_play.setOnClickListener(v -> {
+            if (started) {
+                started = false;
+                mediaPlayer.pause();
+                text_status.setText("PLAY");
+
+            } else {
+                started = true;
+                mediaPlayer.start();
+                text_status.setText("PAUSE");
+
             }
         });
+
+
         return root;
+    }
+
+    class PlayerTask extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            try{
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.prepare();
+                prepared = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            btn_play.setEnabled(true);
+            text_status.setText("PLAY");
+        }
+
+
+
+
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(started) {
+            mediaPlayer.pause();
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(started) {
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+
+        super.onDestroy();
+        if(prepared) {
+            mediaPlayer.release();
+        }
+
+
     }
 }
