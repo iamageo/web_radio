5ca424217c28fb1b23130f9e159dfc879b1bac9c
iamageo <geovani.af4@gmail.com>
2021-04-25 16:38:41 -0300

    implement audiomanager vol

--- app/src/main/java/com/iamageo/conexaopororocav1/ui/home/HomeFragment.java
+++ app/src/main/java/com/iamageo/conexaopororocav1/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.iamageo.conexaopororocav1.ui.home;
 
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
@@ -20,10 +21,14 @@ import com.iamageo.conexaopororocav1.R;
 
 import java.io.IOException;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
 public class HomeFragment extends Fragment {
 
-    //botão pay
+    //botão play, mais e menos
     private Button btn_play;
+    private Button btn_mais;
+    private Button btn_menos;
 
     //linear layout play e progress bar
     private ProgressBar progressBarLoading;
@@ -44,38 +49,59 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        audioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
 
-        btn_play = root.findViewById(R.id.btn_play);
-        progressBarLoading = root.findViewById(R.id.pb_loading);
-        linearLayout = root.findViewById(R.id.linearLayout);
+        //inicializando components do findviewbyid
+        initComponents(root);
 
+        //escondendo o linear layout
         linearLayout.setVisibility(View.INVISIBLE);
 
+        //variáveis do media player
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
         new PlayerTask().execute(stream);
 
+        //botão play e pause
         btn_play.setOnClickListener(v -> {
             if (started) {
                 started = false;
                 mediaPlayer.pause();
                 btn_play.setText("PLAY");
-
-
             } else {
                 started = true;
                 mediaPlayer.start();
                 btn_play.setText("PAUSE");
-
             }
+        });
+
+        //botão volume mais
+        btn_mais.setOnClickListener(v -> {
+            audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+        });
+
+        //botão volume menos
+        btn_menos.setOnClickListener(v->{
+            audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
         });
 
         return root;
     }
 
-    class PlayerTask extends AsyncTask<String, Void, Boolean> {
 
+    //função que iicializa os componentes da ui
+    public void initComponents(View view) {
+        btn_play = view.findViewById(R.id.btn_play);
+        btn_mais = view.findViewById(R.id.btn_mais);
+        btn_menos = view.findViewById(R.id.btn_menos);
+
+        progressBarLoading = view.findViewById(R.id.pb_loading);
+        linearLayout = view.findViewById(R.id.linearLayout);
+    }
+
+    //carregando stream
+    class PlayerTask extends AsyncTask<String, Void, Boolean> {
         @Override
         protected Boolean doInBackground(String... strings) {
             try{
@@ -88,16 +114,23 @@ public class HomeFragment extends Fragment {
             return prepared;
         }
 
+        //apos o link ser carregado
         @Override
         protected void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
+
+            //tornando linear layout visível
             progressBarLoading.setVisibility(View.INVISIBLE);
             linearLayout.setVisibility(View.VISIBLE);
+
+            //setando textos do botão
             btn_play.setEnabled(true);
             btn_play.setText("PLAY");
         }
 
     }
+
+    //métodos do ciclo de vida - modificar
     @Override
     public void onPause() {
         super.onPause();
@@ -105,7 +138,6 @@ public class HomeFragment extends Fragment {
             mediaPlayer.pause();
         }
     }
-
 
     @Override
     public void onResume() {
@@ -121,6 +153,5 @@ public class HomeFragment extends Fragment {
         if(prepared) {
             mediaPlayer.release();
         }
-
     }
 }
